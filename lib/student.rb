8dc7bb5539c76b786c83d433ecea7614d58d4476
Monica R <mlisareyes@gmail.com>
2019-11-06 02:46:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,13 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+  attr_accessor :name, :grade, :id
+
+  def initialize(name, grade, id)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
 end
